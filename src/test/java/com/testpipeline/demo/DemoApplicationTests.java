dce0c58f787350f555ec5fd974b8a7da8687e16f
jehanit14 <jehanit14@gmail.com>
2021-05-19 22:12:10 +0530

    second commit

---
+++ src/test/java/com/testpipeline/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testpipeline.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
